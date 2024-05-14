cd61af7c97c49790883d048746bf07d0a3170815
Michael <michael@n90x.info>
2024-05-14 16:47:28 +0200

    Implement import data function with the capability to retrieve the amount of rows.

--- School_Scheduler/School_Scheduler.cpp
+++ School_Scheduler/School_Scheduler.cpp
@@ -17,14 +17,26 @@ School_Scheduler::~School_Scheduler() {
 // Get the needed data to import user data from file.
 void School_Scheduler::import_data() {
     bool success;                                                                                      // success flag
-    QString text = QInputDialog::getText(this,                                                         // parent
-                                        "Import Data.",                                                // title
-                                        "Please select the amount and the file you want to import.",   // text in the input dialog
-                                        QLineEdit::Normal,                                             // text format in the input - user can see the text
-                                        "inputfile.txt",                                               // default file name
-                                        &success);                                                     // if user pressed 'OK' or 'Cancel'
+    QString fileName = QInputDialog::getText(this,                                                         // parent
+                                            "Import Data.",                                                // title
+                                            "Please enter the name of the file you want to import.",       // text in the input dialog
+                                            QLineEdit::Normal,                                             // text format in the input - user can see the text
+                                            "inputfile.txt",                                               // default file name
+                                            &success);                                                     // if user pressed 'OK' or 'Cancel'
 
-    if (success && text.isEmpty())
+    if (success && fileName.isEmpty()) {
+        // fileName.toStdString().c_str();
+        cout << "Empty text!" << endl;
+    }
+
+    int numberOfItems = QInputDialog::getInt(this,
+                                            "Import Data.",                                                // title
+                                            "How many row do you want to import?", 
+                                            5,
+                                            1,
+                                            10);
+
+    if (success)
         cout << "Empty text!" << endl;
 
 }
