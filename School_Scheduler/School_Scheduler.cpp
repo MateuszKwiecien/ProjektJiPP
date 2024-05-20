34f9286cfed5c5c56c0a016efc12c841a961db99
Michael <michael@n90x.info>
2024-05-20 17:35:51 +0200

    Implement button functionality to view imported data.

--- School_Scheduler/School_Scheduler.cpp
+++ School_Scheduler/School_Scheduler.cpp
@@ -2,6 +2,8 @@
 #include <QMessageBox>
 #include <QInputDialog>
 #include <QFileDialog.h>
+#include <QLabel>
+#include <QListWidget>
 #include <iostream>
 
 using namespace std;
@@ -32,8 +34,49 @@ void School_Scheduler::import_data() {
     processFile(fileName, numberOfItems);
 }
 
-
+// Show the imported data by the user.
 void School_Scheduler::show_import() {
+    // Create a new QDialog instance for displaying the imported data.
+    QDialog* lookAtDataDialog = new QDialog();
+    
+    // Set the dialog to be application modal, so it blocks input to other windows in the application.
+    lookAtDataDialog->setWindowModality(Qt::WindowModality::ApplicationModal);
+
+    // Set fixed dimensions for the dialog.
+    lookAtDataDialog->setMinimumHeight(400);
+    lookAtDataDialog->setMinimumWidth(500);
+    lookAtDataDialog->setMaximumHeight(400);
+    lookAtDataDialog->setMaximumWidth(500);
+
+    // Create a new QLabel instance for the title.
+    QLabel* titleLabel = new QLabel();
+
+
+    titleLabel->setText("This is the data You imported.");
+    // 50px from the left (with all these parameters 50px will be from the left and right).
+    // 20px from the top
+    // 400px width
+    // 20px height
+    titleLabel->setGeometry(50, 25, 400, 20);              
+    titleLabel->setStyleSheet("QLabel { background-color : red; }");
+    titleLabel->setAlignment(Qt::AlignCenter);
+    titleLabel->setParent(lookAtDataDialog);
+    titleLabel->show();                         
+
+    // Create a new QListWidget instance for displaying the data.
+    QListWidget* dataListWidget = new QListWidget();
+
+    dataListWidget->setGeometry(50, 70, 400, 300);
+    dataListWidget->setParent(lookAtDataDialog);
+
+    // Dummy data.
+    for (int i = 0; i < 100; i++) {
+        dataListWidget->addItem(QString::number(i));
+    }
+
+    dataListWidget->show();
+
+    lookAtDataDialog->show();
 
 }
 
